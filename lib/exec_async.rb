52908796f11201de2e4b79a05f90182f4f787edf
Jorge Rodriguez <jorge.rodriguez.suarez@gmail.com>
2025-04-08 09:48:36 -0400

    Add options to customize sidekiq jobs

--- lib/exec_async.rb
+++ lib/exec_async.rb
@@ -25,25 +25,29 @@ module ExecAsync
       [name, id, method_name.to_s, args]
     end
 
-    def exec_async(method_name, *args)
-      ExecAsyncJob
+    def exec_async(method_name, *args, options: { queue: :default })
+      ExecAsyncWorker
+        .set(options)
         .perform_async(*extract_args(self, method_name, args))
     end
 
-    def exec_async_in(seconds, method_name, *args)
-      ExecAsyncJob
+    def exec_async_in(seconds, method_name, *args, options: { queue: :default })
+      ExecAsyncWorker
+        .set(options)
         .perform_in(seconds,
                     *extract_args(self, method_name, args))
     end
   end
 
-  def exec_async(method_name, *args)
-    ExecAsyncJob
+  def exec_async(method_name, *args, options: { queue: :default })
+    ExecAsyncWorker
+      .set(options)
       .perform_async(*self.class.extract_args(self, method_name, args))
   end
 
-  def exec_async_in(seconds, method_name, *args)
-    ExecAsyncJob
+  def exec_async_in(seconds, method_name, *args, options: { queue: :default })
+    ExecAsyncWorker
+      .set(options)
       .perform_in(seconds,
                   *self.class.extract_args(self, method_name, args))
   end
